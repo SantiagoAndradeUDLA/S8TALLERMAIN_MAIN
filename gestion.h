f657838dc5b1a5e16d3235b5323d7ee7319039fd
SantiagoAndradeUDLA <santiago.andrade.bucheli@udla.edu.ec>
2024-11-21 13:27:20 -0500

    Create gestion.h

---
+++ gestion.h
@@ -0,0 +1,12 @@
+#ifndef GESTION_H
+#define GESTION_H
+
+#define MAX_PRODUCTOS 100
+#define MAX_NOMBRE 50
+
+void agregar_producto(char (*nombres)[MAX_NOMBRE], int *cantidades, int *pesos, float *precios, int *tiempos_fabricacion, int *total_productos);
+void editar_producto(char (*nombres)[MAX_NOMBRE], int *cantidades, int *pesos, float *precios, int *tiempos_fabricacion, int total_productos);
+void eliminar_producto(char (*nombres)[MAX_NOMBRE], int *cantidades, int *pesos, float *precios, int *tiempos_fabricacion, int *total_productos);
+void mostrar_resumen(char (*nombres)[MAX_NOMBRE], int *cantidades, int *pesos, float *precios, int *tiempos_fabricacion, int total_productos);
+
+#endif
